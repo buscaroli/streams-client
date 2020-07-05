56a3f8cfd54484e458441db81f85483d9516d6e9
buscaroli <m.buscaroli@outlook.com>
2020-07-05 10:39:27 +0100

    Fixed StreamShow and StreamList Components, added Switch from React-Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Router, Route } from 'react-router-dom'
+import { Router, Route, Switch } from 'react-router-dom'
 import StreamCreate from './streams/StreamCreate'
 import StreamList from './streams/StreamList'
 import StreamShow from './streams/StreamShow'
@@ -13,13 +13,15 @@ const App = () => {
     return (
         <div className="ui container">
             <Router history={history}>
-                <div>
+                <div> 
                     <Header />
-                    <Route path="/" exact component={StreamList} />
-                    <Route path="/streams/show" component={StreamShow} />
-                    <Route path="/streams/new" component={StreamCreate} />
-                    <Route path="/streams/delete/:id" component={StreamDelete} />
-                    <Route path="/streams/edit/:id" component={StreamEdit} />
+                    <Switch>
+                        <Route path="/" exact component={StreamList} />   
+                        <Route path="/streams/new" exact component={StreamCreate} />
+                        <Route path="/streams/edit/:id" exact component={StreamEdit} />
+                        <Route path="/streams/delete/:id" exact component={StreamDelete} />
+                        <Route path="/streams/:id" exact component={StreamShow} />
+                    </Switch>
                 </div>
             </Router>
         </div>      
